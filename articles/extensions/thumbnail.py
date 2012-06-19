da9635a3d983694b1307b9d1a561c814f99b49eb
Matthias Kestenholz <mk@spinlock.ch>
2012-06-19 10:41:43 +0200

    Rewrite extensions using feincms.extensions

--- articles/extensions/thumbnail.py
+++ articles/extensions/thumbnail.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-def register(cls, admin_cls):
-    cls.add_to_class('thumbnail', models.ImageField(_('thumbnail'), max_length=250, upload_to="images/articles/thumbnails", null=True, blank=True))
-    if admin_cls:
-        if admin_cls.fieldsets:
-            admin_cls.fieldsets[0][1]['fields'].append('thumbnail')
+from feincms import extensions
 
 
+class Extension(extensions.Extension):
+    def handle_model(self):
+        self.model.add_to_class('thumbnail',
+            models.ImageField(_('thumbnail'), max_length=250,
+            upload_to="images/articles/thumbnails", null=True, blank=True))
+
+    def handle_modeladmin(self, modeladmin):
+        modeladmin.add_extension_options('thumbnail')
